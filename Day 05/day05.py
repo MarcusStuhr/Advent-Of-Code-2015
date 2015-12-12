f5020e9656d4014fe86e6e3f888ad3d939c81785
MANGO\Marcus <marcus.stuhr@gmail.com>
2015-12-12 14:17:05 -0500

    Uploading solution to day 5

---
+++ Day 05/day05.py
@@ -0,0 +1,64 @@
+import sys
+import re
+
+
+PUZZLE_DATA_FILENAME = "day05_input.txt"
+
+
+def is_nice_part_1(s):
+    """
+    Returns True if string s is a nice string using criteria from part 1.
+    Returns False otherwise.
+    Uses a three-part regex:
+    1. s does not contain "ab", "cd", "pq", or "xy"
+    2. s has at least three vowels
+    3. s contains a duplicated-letter pair
+    """
+    return all([(not re.search(r"(ab|cd|pq|xy)", s)),  len(re.findall(r"[aeiou]", s)) >= 3, re.search(r"(.)\1", s)])
+
+
+def is_nice_part_2(s):
+    """
+    Returns True if string s is a nice string using criteria from part 1.
+    Returns False otherwise.
+    Uses a two-part regex:
+    1. s contains a letter triplet where the first and third characters match. Second char can be anything.
+    2. s contains at least two identical, non-overlapping letter pairs
+    """
+    return all([re.search(r"(.).\1", s), re.search(r"(..).*\1", s)])
+
+
+def count_nice_strings_part_1(strings):
+    """
+    Sums up the count of strings that fulfill the nice-string criteria from part 1
+    """
+    return sum(is_nice_part_1(s) for s in strings)
+
+
+def count_nice_strings_part_2(strings):
+    """
+    Sums up the count of strings that fulfill the nice-string criteria from part 2
+    """
+    return sum(is_nice_part_2(s) for s in strings) 
+
+
+def get_file_input(filename):
+    try:
+        with open(filename) as f:
+            data = f.read()
+        return data
+    except IOError:
+       print "Unable to open/read input file {}".format(filename)
+       sys.exit(1)
+
+
+def main():
+    file_contents_string = get_file_input(PUZZLE_DATA_FILENAME)
+    strings = file_contents_string.split('\n')
+
+    print "Answer to part 1: {}".format(count_nice_strings_part_1(strings))
+    print "Answer to part 2: {}".format(count_nice_strings_part_2(strings))
+
+ 
+if __name__ == "__main__":
+    main()
