be2e556d64aec698c334dd1bb90caa0b39df22e8
MANGO\Marcus <marcus.stuhr@gmail.com>
2015-12-12 12:16:19 -0500

    Uploading solution to day 1

--- Day 01/day01.py
+++ Day 01/day01.py
@@ -1,12 +1,50 @@
 import sys
 
-def part1():
-    with open(sys.argv[1]) as f:
-        instructions = f.read()
-    return instructions
+
+PUZZLE_DATA_FILENAME = "day01_input.txt"
+
+
+def part1(instructions):
+    """
+    Assumed starting position is floor 0.
+    Finds the final floor by counting up the number of ups vs. downs.
+    """
+    return sum(1 if instruction=='(' else -1 for instruction in instructions)
+
+
+def part2(instructions):
+    """
+    Assumed starting position is floor 0.
+    Tracks the up/down movements to determine when floor -1 is reached.
+    Returns -1 if floor -1 is never reached.
+    """
+    current_floor = 0
+    
+    for index,instruction in enumerate(instructions):
+        if   instruction == '(':  current_floor +=1
+        elif instruction == ')':  current_floor -=1
+        
+        if current_floor == -1: return index+1
+        
+    return -1
+
+
+def get_file_input(filename):
+    try:
+        with open(filename) as f:
+            data = f.read()
+        return data
+    except IOError:
+       print "Unable to open/read input file {}".format(filename)
+       sys.exit(1)
+
 
 def main():
-    print part1()
+    instructions = get_file_input(PUZZLE_DATA_FILENAME)
+    
+    print "Answer to part 1: {}".format(part1(instructions))
+    print "Answer to part 2: {}".format(part2(instructions))
 
+ 
 if __name__ == "__main__":
     main()
