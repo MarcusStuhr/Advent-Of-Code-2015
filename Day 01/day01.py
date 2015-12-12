c325c424637c8ca5a122eaee53680054dcf2caee
MANGO\Marcus <marcus.stuhr@gmail.com>
2015-12-12 12:47:18 -0500

    Edited function names

--- Day 01/day01.py
+++ Day 01/day01.py
@@ -4,7 +4,7 @@ import sys
 PUZZLE_DATA_FILENAME = "day01_input.txt"
 
 
-def part1(instructions):
+def final_floor(instructions):
     """
     Assumed starting position is floor 0.
     Finds the final floor by counting up the number of ups vs. downs.
@@ -12,10 +12,11 @@ def part1(instructions):
     return sum(1 if instruction=='(' else -1 for instruction in instructions)
 
 
-def part2(instructions):
+def find_basement_index(instructions):
     """
     Assumed starting position is floor 0.
     Tracks the up/down movements to determine when floor -1 is reached.
+    Returns 1-indexed answer.
     Returns -1 if floor -1 is never reached.
     """
     current_floor = 0
@@ -42,8 +43,8 @@ def get_file_input(filename):
 def main():
     instructions = get_file_input(PUZZLE_DATA_FILENAME)
     
-    print "Answer to part 1: {}".format(part1(instructions))
-    print "Answer to part 2: {}".format(part2(instructions))
+    print "Answer to part 1: {}".format(final_floor(instructions))
+    print "Answer to part 2: {}".format(find_basement_index(instructions))
 
  
 if __name__ == "__main__":
