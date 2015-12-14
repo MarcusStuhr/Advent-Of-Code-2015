fc202acf2650372e8d30ad9493c4992a9e3dc19b
Marcus Stuhr <marcus.stuhr@gmail.com>
2015-12-14 15:39:22 -0500

    Update day09.py

--- Day 09/day09.py
+++ Day 09/day09.py
@@ -1,52 +1,45 @@
 import sys
 from collections import defaultdict
 
+
 PUZZLE_DATA_FILENAME = "day09_input.txt"
 
 memo_cache = {}
 
-def hamiltonian_min_and_max_cost(costs_graph, node_names, from_node, already_visited):
-    """
-    Computes the min and max-costs of the Hamiltonian paths starting at from_node, without
-    visiting any nodes encoded in the bitmask already_visited. This function is memoized on
-    (from_node, already_visited) with an external dictionary called memo_cache.
-    
-    Inputs:
-    costs_graph: A defaultdict of costs. costs_graph[A][B] is the cost of going from A to B directly
-    node_names: A list of the node names, derived from the keys of costs_graph
-    from_node: The name of the node we are about to leave
-    already_visited: A bitmask representing which nodes we've already visited (mapping to node_names)
 
-    Assumptions:
-    -The value 10**10 is larger than any of the path costs, and -10**10 is smaller, similarly
-    
-    Returns:
-    A tuple, (is_valid, min_cost, max_cost)
-    is_valid is True if a Hamiltonian path was found, and False otherwise.
+def hamiltonian_path_cost(costs_graph, from_node, already_visited):
+    if already_visited == (1<<len(costs_graph.keys()))-1: #if all nodes have been visited, we stop
+        return True, 0, 0
 
-    Time complexity:
-    O(n^2 * 2^n), where n is the number of nodes
-    """
-    
-    if already_visited == (1<<len(node_names))-1: #if all nodes have been visited, we stop
-        return True,0,0
-    
     memo_key = (from_node, already_visited)
     if memo_key in memo_cache: #if we've encountered this position before, return the work we've already done
-        return memo_cache[memo_key] 
-    
+        return memo_cache[memo_key]
+
     min_cost = 10**10
     max_cost = -10**10
     is_valid = False
-    
-    for index,to_node in enumerate(node_names):
+
+    for index, to_node in enumerate(costs_graph.keys()):
         if already_visited & (1<<index) == 0 and to_node in costs_graph[from_node]:
-            is_valid, min_cost_next, max_cost_next = hamiltonian_min_and_max_cost(costs_graph, node_names, to_node, already_visited | (1<<index))
+            is_valid, min_cost_next, max_cost_next = hamiltonian_path_cost(costs_graph, to_node, already_visited | (1<<index))
             if is_valid:
                 min_cost = min(min_cost, costs_graph[from_node][to_node] + min_cost_next)
                 max_cost = max(max_cost, costs_graph[from_node][to_node] + max_cost_next)
-            
+
     memo_cache[memo_key] = (is_valid, min_cost, max_cost)
+    return is_valid, min_cost, max_cost
+
+
+def find_hamiltonian_min_max_costs(costs_graph):
+    min_cost = 10**10
+    max_cost = -10**10
+    is_valid = False
+
+    for index, to_node in enumerate(costs_graph.keys()):
+        is_valid, min_cost_next, max_cost_next = hamiltonian_path_cost(costs_graph, to_node, 1<<index)
+        if is_valid:
+            min_cost = min(min_cost, min_cost_next)
+            max_cost = max(max_cost, max_cost_next)
     return is_valid, min_cost, max_cost
 
 
@@ -65,23 +58,15 @@ def main():
     lines = file_contents_string.split('\n')
 
     dist_graph = defaultdict(dict)
-    
+
     for line in lines:
         words = line.strip().split(" ")
         from_city,to_city,dist = words[0],words[2],int(words[-1])
         dist_graph[from_city][to_city] = dist
         dist_graph[to_city][from_city] = dist
-        
-    min_cost = 10**10
-    max_cost = -10**10
-    city_names = list(dist_graph.keys())
-    
-    for index, to_city in enumerate(city_names):
-        is_valid, min_cost_next, max_cost_next = hamiltonian_min_and_max_cost(dist_graph, city_names, to_city, 1<<index)
-        if is_valid:
-            min_cost = min(min_cost, min_cost_next)
-            max_cost = max(max_cost, max_cost_next)
-    
+
+    is_valid, min_cost, max_cost = find_hamiltonian_min_max_costs(dist_graph)
+
     print "Answer to part 1: {}".format(min_cost)
     print "Answer to part 2: {}".format(max_cost)
 
