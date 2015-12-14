348461760b72dbdafb34e478ab7795591922f4f1
MANGO\Marcus <marcus.stuhr@gmail.com>
2015-12-13 22:40:19 -0500

    Uploading solution to day 10

---
+++ Day 10/day10.py
@@ -0,0 +1,18 @@
+import sys
+from itertools import groupby
+
+
+def look_and_say(input_string, num_iterations):
+    for i in xrange(num_iterations):
+        input_string = ''.join([str(len(list(g))) + str(k) for k, g in groupby(input_string)])
+    return input_string
+
+
+def main():
+    initial_string = "1113122113"
+    print "Answer to part 1: {}".format(len(look_and_say(initial_string, 40)))
+    print "Answer to part 2: {}".format(len(look_and_say(initial_string, 50)))
+
+ 
+if __name__ == "__main__":
+    main()
