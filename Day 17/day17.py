fcb628ee20d2bf3fcf327c24a7fb699a61738eee
MANGO\Marcus <marcus.stuhr@gmail.com>
2015-12-17 00:47:57 -0500

    Switched to dynamic programming solution

--- Day 17/day17.py
+++ Day 17/day17.py
@@ -4,13 +4,22 @@ from itertools import combinations
 PUZZLE_DATA_FILENAME = "day17_input.txt"
 
 
-def count_combinations(container_sizes, target_sum, find_minimal = False):
+def count_combinations(container_sizes, target_sum):
+    dp = [1] + [0] * (target_sum)
+    for cur_num in container_sizes:
+        for next_num in xrange(target_sum - cur_num, -1, -1):
+            if dp[next_num]:
+                dp[next_num + cur_num] += dp[next_num]
+    return dp[target_sum]
+
+
+def count_minimal_combinations(container_sizes, target_sum):
     ans = 0
-    for k in xrange(1, len(container_sizes)+1):
+    for k in xrange(1, len(container_sizes) + 1):
         for c in combinations(container_sizes, k):
             if sum(c) == target_sum:
                 ans+=1
-        if find_minimal == True and ans:
+        if ans:
             break
     return ans
 
@@ -28,7 +37,7 @@ def get_file_input(filename):
 def main():
     container_sizes = map(int,get_file_input(PUZZLE_DATA_FILENAME).split("\n"))
     print "Answer to part 1: {}".format(count_combinations(container_sizes, 150))
-    print "Answer to part 2: {}".format(count_combinations(container_sizes, 150, True))
+    print "Answer to part 2: {}".format(count_minimal_combinations(container_sizes, 150))
 
 
 if __name__ == "__main__":
