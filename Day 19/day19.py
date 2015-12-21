af5827034758c1eed27e653ba9076a6d71ed2286
Marcus Stuhr <marcus.stuhr@gmail.com>
2015-12-21 15:44:12 -0500

    Added faster method for part 2

--- Day 19/day19.py
+++ Day 19/day19.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from random import shuffle
+import re
 
 PUZZLE_DATA_FILENAME = "day19_input.txt"
 
@@ -12,6 +13,7 @@ def count_distinct_molecules(transitions, medicine_molecule):
                 distinct_molecules.add(new_molecule)
     return len(distinct_molecules)
 
+'''
 def count_steps(transitions, medicine_molecule):
     phrase = medicine_molecule
     num_steps = 0
@@ -28,6 +30,11 @@ def count_steps(transitions, medicine_molecule):
 
 def count_steps_min(transitions, medicine_molecule, num_trials = 20):
     return min(count_steps(transitions, medicine_molecule) for i in xrange(num_trials))
+'''
+
+def count_steps_fast(medicine_molecule):
+    s = medicine_molecule
+    return len(re.findall(r"[A-Z][a-z]?",s)) - 2*s.count("Rn") - 2*s.count("Y") - 1
 
 def main():
     lines = [line.split(" ") for line in open(PUZZLE_DATA_FILENAME).read().split("\n")]
@@ -36,7 +43,7 @@ def main():
         transitions.append((before,after))
     medicine_molecule = lines[-1][0]
     print "Answer to part 1: {}".format(count_distinct_molecules(transitions, medicine_molecule))
-    print "Answer to part 2: {}".format(count_steps_min(transitions, medicine_molecule))
+    print "Answer to part 2: {}".format(count_steps_fast(medicine_molecule))
 
 
 if __name__ == "__main__":
